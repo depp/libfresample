fffe24981bd778a116f22f342fafdcaf87f08a28
Dietrich Epp <depp@zdome.net>
2012-10-13 20:06:41 -0700

    Clean up resample_s16n1s16_sse2

--- lib/resample_s16n1s16_sse2.c
+++ lib/resample_s16n1s16_sse2.c
@@ -4,55 +4,7 @@
 #include "cpu.h"
 #if defined(LFR_CPU_X86)
 #include "resample.h"
-#include <stdint.h>
-
-#define UNALIGNED_LOAD 1
-
-#define LOOP_LOADFIR \
-    fir0 = firp[(fn+0)*flen + i];               \
-    fir1 = firp[(fn+1)*flen + i];               \
-    fir0 = _mm_packs_epi32(                     \
-        _mm_srai_epi32(                         \
-            _mm_madd_epi16(                     \
-                _mm_unpacklo_epi16(fir0, fir1), \
-                fir_interp),                    \
-            INTERP_BITS),                       \
-        _mm_srai_epi32(                         \
-            _mm_madd_epi16(                     \
-                _mm_unpackhi_epi16(fir0, fir1), \
-                fir_interp),                    \
-            INTERP_BITS))
-
-#define LOOP_ACCUM \
-    acc = _mm_add_epi32(acc, _mm_madd_epi16(dat0, fir0))
-
-#define LOOP_ALIGN0 \
-    for (i = fidx0; i < fidx1; ++i) {   \
-        dat0 = inp[(off >> 3) + i + 0]; \
-        LOOP_LOADFIR;                   \
-        LOOP_ACCUM;                     \
-    }
-
-#define LOOP_ALIGN(n) \
-    dat1 = inp[(off >> 3) + fidx0];             \
-    for (i = fidx0; i < fidx1; ++i) {           \
-        dat0 = dat1;                            \
-        dat1 = inp[(off >> 3) + i + 1];         \
-        dat0 = _mm_or_si128(                    \
-            _mm_srli_si128(dat0, (n)*2),        \
-            _mm_slli_si128(dat1, 16-(n)*2));    \
-        LOOP_LOADFIR;                           \
-        LOOP_ACCUM;                             \
-    }
-
-#define LOOP_UNALIGNED \
-    for (i = fidx0; i < fidx1; ++i) {           \
-        dat0 = _mm_loadu_si128(                 \
-            (const __m128i *)                   \
-            ((const short *) inp + off + i*8)); \
-        LOOP_LOADFIR;                           \
-        LOOP_ACCUM;                             \
-    }
+#include <string.h>
 
 void
 lfr_resample_s16n1s16_sse2(
@@ -60,19 +12,14 @@ lfr_resample_s16n1s16_sse2(
     void *out, int outlen, const void *in, int inlen,
     const struct lfr_filter *filter)
 {
-    const __m128i *firp, *inp;
-    __m128i *outp;
-    int in0, in1, out0, out1, outidx;
+    const __m128i *fd;
     int flen, log2nfilt;
     lfr_fixed_t x;
 
     __m128i acc, acc0, acc1, acc2, fir0, fir1, fir_interp, dat0;
     __m128i dsv, lcg_a, lcg_c;
-#if !UNALIGNED_LOAD
-    __m128i dat1;
-#endif
-    int fn, ff0, ff1, off0, off, fidx0, fidx1;
-    int accs, i, f, t;
+    int fn, ff0, ff1, off, fidx0, fidx1;
+    int accs, i, j, f;
     unsigned ds;
 
     union {
@@ -80,30 +27,15 @@ lfr_resample_s16n1s16_sse2(
         __m128i x;
     } u;
 
-    /* firp: Pointer to beginning of filter coefficients, aligned.  */
-    firp = (const __m128i *) filter->data;
+    /* fd: Pointer to beginning of filter coefficients, aligned.  */
+    fd = filter->data;
     /* flen: Length of filter, measured in 128-bit words.  */
     flen = filter->nsamp >> 3;
-    off0 = filter->nsamp >> 1;
     /* log2nfilt: Base 2 logarithm of the number of filters.  */
     log2nfilt = filter->log2nfilt;
+    x = *pos - ((lfr_fixed_t) (filter->nsamp >> 1) << 32);
 
-    /* in0, in1: Sample index of input start and end, measured from
-       aligned input pointer.  inp: aligned input pointer.  */
-    in0 = ((uintptr_t) in >> 1) & 7;
-    in1 = inlen + in0;
-    inp = (const __m128i *) ((const char *) in - in0 * 2);
-
-    /* out0, out1: Sample index of output start and end, measured from
-       aligned output pointer.  outp: aligned output pointer.  */
-    out0 = ((uintptr_t) out >> 1) & 7;
-    out1 = outlen + out0;
-    outp = (__m128i *) ((char *) out - out0 * 2);
-
-    x = *pos + ((lfr_fixed_t) in0 << 32);
     ds = *dither;
-    for (i = 0; i < (out0 & 7); ++i)
-        ds = LCG_AI * ds + LCG_CI;
     for (i = 0; i < 4; ++i) {
         u.d[i] = ds;
         ds = ds * LCG_A + LCG_C;
@@ -114,12 +46,7 @@ lfr_resample_s16n1s16_sse2(
 
     acc0 = _mm_set1_epi32(0);
     acc1 = _mm_set1_epi32(0);
-    for (outidx = out0; outidx < out1; ++outidx) {
-        /* acc: FIR accumulator, as four 32-bit words.  The four words
-           must be added, shifted, and packed before they can be
-           stored.  */
-        acc = _mm_set1_epi32(0);
-
+    for (i = 0; i < outlen; ++i) {
         /* fn: filter number
            ff0: filter factor for filter fn
            ff1: filter factor for filter fn+1 */
@@ -130,24 +57,26 @@ lfr_resample_s16n1s16_sse2(
         ff0 = (1u << INTERP_BITS) - ff1;
         fir_interp = _mm_set1_epi32(ff0 | (ff1 << 16));
 
+        /* acc: FIR accumulator, as four 32-bit words.  The four words
+           must be added, shifted, and packed before they can be
+           stored.  */
+        acc = _mm_set1_epi32(0);
         /* off: offset in input corresponding to first sample in
            filter */
-        off = (int) (x >> 32) - off0;
-
+        off = (int) (x >> 32);
         /* fixd0, fidx1: start, end indexes of 8-word (16-byte) chunks
            of whole FIR data we will use */
-        fidx0 = (in0 - off + 7) >> 3;
-        fidx1 = (in1 - off) >> 3;
+        fidx0 = (-off + 7) >> 3;
+        fidx1 = (inlen - off) >> 3;
         if (fidx0 > 0) {
             if (fidx0 > flen)
                 goto accumulate;
             accs = 0;
-            t = (off > in0) ? off : in0;
-            for (i = t; i < fidx0 * 8 + off; ++i) {
-                f = (((const short *) firp)[(fn+0)*flen*8 + i - off] * ff0 +
-                     ((const short *) firp)[(fn+1)*flen*8 + i - off] * ff1)
+            for (j = -off; j < fidx0 * 8; ++j) {
+                f = (((const short *) fd)[(fn+0) * (flen*8) + j] * ff0 +
+                     ((const short *) fd)[(fn+1) * (flen*8) + j] * ff1)
                     >> INTERP_BITS;
-                accs += ((const short *) inp)[i] * f;
+                accs += ((const short *) in)[j + off] * f;
             }
             acc = _mm_cvtsi32_si128(accs);
         } else {
@@ -157,35 +86,39 @@ lfr_resample_s16n1s16_sse2(
             if (fidx1 < 0)
                 goto accumulate;
             accs = 0;
-            t = (off + flen*8 < in1) ? (off + flen*8) : in1;
-            for (i = fidx1 * 8 + off; i < t; ++i) {
-                f = (((const short *) firp)[(fn+0)*flen*8 + i - off] * ff0 +
-                     ((const short *) firp)[(fn+1)*flen*8 + i - off] * ff1)
+            for (j = fidx1 * 8; j < inlen - off; ++j) {
+                f = (((const short *) fd)[(fn+0) * (flen*8) + j] * ff0 +
+                     ((const short *) fd)[(fn+1) * (flen*8) + j] * ff1)
                     >> INTERP_BITS;
-                accs += ((const short *) inp)[i] * f;
+                accs += ((const short *) in)[j + off] * f;
             }
             acc = _mm_add_epi32(acc, _mm_cvtsi32_si128(accs));
         } else {
             fidx1 = flen;
         }
 
-#if UNALIGNED_LOAD
-        LOOP_UNALIGNED;
-#else
-        switch (off & 7) {
-        case 0: LOOP_ALIGN0; break;
-        case 1: LOOP_ALIGN(1); break;
-        case 2: LOOP_ALIGN(2); break;
-        case 3: LOOP_ALIGN(3); break;
-        case 4: LOOP_ALIGN(4); break;
-        case 5: LOOP_ALIGN(5); break;
-        case 6: LOOP_ALIGN(6); break;
-        case 7: LOOP_ALIGN(7); break;
+        for (j = fidx0; j < fidx1; ++j) {
+            dat0 = _mm_loadu_si128(
+                (const __m128i *)
+                ((const short *) in + off + j*8));
+            fir0 = fd[(fn+0)*flen + j];
+            fir1 = fd[(fn+1)*flen + j];
+            fir0 = _mm_packs_epi32(
+                _mm_srai_epi32(
+                    _mm_madd_epi16(
+                        _mm_unpacklo_epi16(fir0, fir1),
+                        fir_interp),
+                    INTERP_BITS),
+                _mm_srai_epi32(
+                    _mm_madd_epi16(
+                        _mm_unpackhi_epi16(fir0, fir1),
+                        fir_interp),
+                    INTERP_BITS));
+            acc = _mm_add_epi32(acc, _mm_madd_epi16(dat0, fir0));
         }
-#endif
 
     accumulate:
-        switch (outidx & 7) {
+        switch (i & 7) {
         case 0: case 2: case 4: case 6:
             acc0 = acc;
             break;
@@ -213,20 +146,15 @@ lfr_resample_s16n1s16_sse2(
                 _mm_unpacklo_epi64(acc1, acc0),
                 _mm_unpackhi_epi64(acc1, acc0));
 
-            /* Apply dither */
             acc2 = _mm_add_epi32(acc2, _mm_srli_epi32(dsv, 17));
             dsv = lfr_rand_epu32(dsv, lcg_a, lcg_c);
             acc1 = _mm_add_epi32(acc1, _mm_srli_epi32(dsv, 17));
             dsv = lfr_rand_epu32(dsv, lcg_a, lcg_c);
-                    
+
             acc = _mm_packs_epi32(
                 _mm_srai_epi32(acc2, 15),
                 _mm_srai_epi32(acc1, 15));
-            if (outidx - out0 >= 7)
-                *outp = acc;
-            else
-                lfr_storepartial_epi16(outp, acc, out0 & 7, 8);
-            outp += 1;
+            _mm_storeu_si128((__m128i *) ((short *) out + i - 7), acc);
             break;
         }
 
@@ -234,17 +162,17 @@ lfr_resample_s16n1s16_sse2(
     }
 
     ds = _mm_cvtsi128_si32(dsv);
-    for (i = 0; i < (out1 & 7); ++i)
+    for (i = 0; i < (outlen & 7); ++i)
         ds = LCG_A * ds + LCG_C;
-    *pos = x - ((lfr_fixed_t) in0 << 32);
+    *pos = x + ((lfr_fixed_t) (filter->nsamp >> 1) << 32);
     *dither = ds;
 
     /* Store remaing bytes */
     acc = _mm_set1_epi32(0);
-    if ((outidx & 7) == 0)
+    if ((outlen & 7) == 0)
         return;
-    for (; ; ++outidx) {
-        switch (outidx & 7) {
+    for (i = outlen; ; ++i) {
+        switch (i & 7) {
         case 0: case 2: case 4: case 6:
             acc0 = acc;
             break;
@@ -280,9 +208,11 @@ lfr_resample_s16n1s16_sse2(
             acc = _mm_packs_epi32(
                 _mm_srai_epi32(acc2, 15),
                 _mm_srai_epi32(acc1, 15));
-            lfr_storepartial_epi16(
-                outp, acc,
-                (unsigned) (out0 ^ out1) < 8 ? (out0 & 7) : 0, out1 & 7);
+            /* We move acc before we call memcpy... because it seems
+               otherwise, we break some of GCC's escape analysis, and
+               get a 13-15% slowdown!  */
+            u.x = acc;
+            memcpy((short *) out + i - 7, &u, 2 * (outlen & 7));
             return;
         }
     }
