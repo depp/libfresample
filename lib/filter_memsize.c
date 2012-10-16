5da03d8350b173a35867bfad578ba611a4c86b31
Dietrich Epp <depp@zdome.net>
2012-10-15 17:42:46 -0700

    Suppress compiler warning

--- lib/filter_memsize.c
+++ lib/filter_memsize.c
@@ -8,5 +8,6 @@ lfr_filter_memsize(const struct lfr_filter *fp)
     switch (fp->type) {
     case LFR_FTYPE_S16: return sz * 2;
     case LFR_FTYPE_F32: return sz * 4;
+    default: return 0;
     }
 }
