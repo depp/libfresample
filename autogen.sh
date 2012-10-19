f5c0f68c25d9ad226cc78a3e44baa0bfb62e4958
Dietrich Epp <depp@zdome.net>
2012-10-19 06:09:50 -0700

    Fix support for old autotools

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,13 @@ set -e
 python tests/genmake.py --autoconf
 aclocal
 autoconf
-libtoolize
+if test -x "`which libtoolize`" ; then
+    libtoolize
+elif test -x "`which glibtoolize`" ; then
+    glibtoolize
+else
+    echo 1>&2 'error: libtoolize is required'
+    exit 1
+fi
 autoheader
 automake --add-missing
