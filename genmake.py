3718fffb05ea53a0c867b15a32c259e10e556237
Dietrich Epp <depp@zdome.net>
2013-04-22 04:51:06 -0700

    Rework universal binary build system
    The new system uses a wrapper script which calls the main configure
    script several times.  The makefile is split into a part which gets
    duplicated for each architecture (arch.mak.in) and another part which
    doesn't (Makefile.in).  Make is not used recursively, so this new system
    should not run into those particular edge cases.

--- genmake.py
+++ genmake.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from cStringIO import StringIO
 
 ALL_SPECIAL = 'altivec mmx sse sse2 sse3 ssse3 sse4_1 sse4_2'.split()
 
@@ -10,55 +11,42 @@ def get_special(name):
     return None
 
 HEADER = """\
-ifeq ($(arch),)
-include config.mak
-else
-include config.$(arch).mak
-endif
-
-depflags = -MF $@.d -MMD -MP
-depfiles := $(wildcard $(builddir)/*/*.d)
+depfiles := $(wildcard @adir@/*/*.d)
 ifneq ($(depfiles),)
 include $(depfiles)
 endif
 
-all_dirs := $(builddir)/shared $(builddir)/static $(builddir)/tool
-missing_dirs := $(filter-out $(wildcard $(all_dirs)),$(all_dirs))
-$(all_dirs):
+all_dirs@asfx@ := @adir@/shared @adir@/static @adir@/tool
+missing_dirs@asfx@ := $(filter-out $(wildcard $(all_dirs@asfx@)),$(all_dirs@asfx@))
+$(all_dirs@asfx@):
 \tmkdir -p $@
-
-ifeq ($(UNAME_S),Darwin)
-CCSHARED :=
-else
-CCSHARED := -fpic
-endif
 
 """
 
-FOOTER = """
+FOOTER = """\
 
 ifeq ($(UNAME_S),Darwin)
 
-$(builddir)/libfresample.a: $(static_obj)
+@adir@/libfresample.a: $(static_obj@asfx@)
 \tlibtool -static -o $@ $^
 
-$(builddir)/$(so_name): $(static_obj)
-\t$(CC) -o $@ -dynamiclib -install_name /Library/Frameworks/fresample.framework/Versions/A/fresample -compatibility_version $(ver_maj).$(ver_min) -current_version $(ver_maj).$(ver_min) $(LDFLAGS) $^ $(LIBS)
+@adir@/$(so_name): $(static_obj@asfx@)
+\t$(CC@asfx@) -o $@ -dynamiclib -install_name /Library/Frameworks/fresample.framework/Versions/A/fresample -compatibility_version $(ver_maj).$(ver_min) -current_version $(ver_maj).$(ver_min) $(LDFLAGS@asfx@) $^ $(LIBS@asfx@)
 
 else
 
-$(builddir)/libfresample.a: $(static_obj)
+@adir@/libfresample.a: $(static_obj@asfx@)
 \trm -f $@
 \tar rc $@ $^
 \tranlib $@
 
-$(builddir)/$(so_name): $(shared_obj)
-\t$(CC) -o $@ -shared -Wl,-soname,$(so_name1) $(LDFLAGS) $^ $(LIBS)
+@adir@/$(so_name): $(shared_obj@asfx@)
+\t$(CC@asfx@) -o $@ -shared -Wl,-soname,$(so_name1) $(LDFLAGS@asfx@) $^ $(LIBS@asfx@)
 
 endif
 
-$(builddir)/fresample: $(tool_obj) $(builddir)/libfresample.a
-\t$(CC) -o $@ $(LDFLAGS) $^ $(LIBS)
+@adir@/fresample: $(tool_obj@asfx@) @adir@/libfresample.a
+\t$(CC@asfx@) -o $@ $(LDFLAGS@asfx@) $^ $(LIBS@asfx@)
 """
 
 def get_sources(dirpath):
@@ -82,6 +70,8 @@ def get_sources(dirpath):
             sources[s] = [path]
     return sources
 
+subst_vars = set('CC CCSHARED LDFLAGS LIBS CFLAGS CWARN CPPFLAGS'.split())
+
 def build_sources(fp, name, sources, **kw):
     cflags = kw.get('cflags', '')
     prefix = kw.get('prefix', '')
@@ -95,43 +85,52 @@ def build_sources(fp, name, sources, **kw):
         if s == 'base':
             s_cflags = ''
         else:
-            s_cflags = '$(%s_CFLAGS)' % s.upper()
+            s_cflags = '$(%s_CFLAGS@asfx@)' % s.upper()
+            subst_vars.add('%s_CFLAGS' % s.upper())
+            subst_vars.add('%s_ENABLED' % s.upper())
 
-        rule = '\t$(CC) -c -o $@ $< -I$(srcdir)/include $(depflags) ' \
-               '$(CPPFLAGS) %s %s $(CWARN) $(CFLAGS)\n' % (s_cflags, cflags)
+        rule = ('\t$(CC@asfx@) -c -o $@ $< -I$(srcdir)/include $(depflags) '
+                '$(CPPFLAGS@asfx@) %s %s $(CWARN@asfx@) $(CFLAGS@asfx@)\n' %
+                (s_cflags, cflags))
 
         objlist = []
         for src in sorted(srclist):
             base = os.path.splitext(os.path.split(src)[1])[0]
-            obj = '$(builddir)/' + prefix + base + '.o'
+            obj = '@adir@/' + prefix + base + '.o'
             src = '$(srcdir)/' + src
             objlist.append(obj)
-            fp.write('%s: %s $(missing_dirs)\n' % (obj, src))
+            fp.write('%s: %s $(missing_dirs@asfx@)\n' % (obj, src))
             fp.write(rule)
 
         objlist = ' '.join(objlist)
         if s == 'base':
-            fp.write('%s := %s\n' % (name, objlist))
+            fp.write('%s@asfx@ := %s\n' % (name, objlist))
         else:
             fp.write(
-                'ifeq ($(%s_ENABLED),1)\n'
-                '%s += %s\n'
+                'ifeq ($(%s_ENABLED@asfx@),1)\n'
+                '%s@asfx@ += %s\n'
                 'endif\n'
                 % (s.upper(), name, objlist))
 
 def run():
-    fp = open('arch.mak', 'w')
+    fp = open('arch.mak.in', 'w')
     fp.write(HEADER)
 
+    fp2 = StringIO()
     lib_src = get_sources('lib')
     tool_src = get_sources('src')
-    build_sources(fp, 'shared_obj', lib_src,
-                  prefix='shared/', cflags='$(CCSHARED)')
-    fp.write('\n')
-    build_sources(fp, 'static_obj', lib_src,
+    build_sources(fp2, 'shared_obj', lib_src,
+                  prefix='shared/', cflags='$(CCSHARED@asfx@)')
+    fp2.write('\n')
+    build_sources(fp2, 'static_obj', lib_src,
                   prefix='static/')
+    fp2.write('\n')
+    build_sources(fp2, 'tool_obj', tool_src, prefix='tool/')
+
+    for var in sorted(subst_vars):
+        fp.write('%s@asfx@ := @%s@\n' % (var, var))
     fp.write('\n')
-    build_sources(fp, 'tool_obj', tool_src, prefix='tool/')
+    fp.write(fp2.getvalue())
 
     fp.write(FOOTER)
     fp.close()
